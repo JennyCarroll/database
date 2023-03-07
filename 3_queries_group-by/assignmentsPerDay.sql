cad0c97511b63b72e797774ab37300f7e1699bad
Jenny Carroll <jennygc@telus.net>
2023-03-07 17:30:51 +0000

    more practice

---
+++ 3_queries_group-by/assignmentsPerDay.sql
@@ -0,0 +1,9 @@
+SELECT day, count(name) AS total_assignments 
+FROM assignments 
+GROUP BY day
+ORDER BY day;
+
+-- SELECT day, count(*) as total_assignments 
+-- FROM assignments
+-- GROUP BY day
+-- ORDER BY day;
