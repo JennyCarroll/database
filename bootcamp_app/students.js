b351be85b894ffee3aedac27bd7a87d43b5d1477
Jenny Carroll <jennygc@telus.net>
2023-03-09 02:48:00 +0000

    update students.js and teachers.js to guard against sql injection

---
+++ bootcamp_app/students.js
@@ -0,0 +1,34 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+// pool.query is a function that accepts an SQL query as a JavaScript string.
+pool
+  .query(queryString, values)
+  // The function then returns a promise that contains our result as an array of objects
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
